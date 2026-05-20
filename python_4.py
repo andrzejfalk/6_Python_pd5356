438ee0d1d06be989d069d39df16f106507dc6fa8
Falkowski Andrzej <a.falkowski.ptmr@gmail.com>
2026-05-20 20:24:49 +0200

    Add files via upload

---
+++ python_4.py
@@ -0,0 +1,21 @@
+# Program pokazuje podstawowe użycie zmiennych w Pythonie
+
+gatunek = "Homo sapiens"
+gen = "BRCA1"
+liczba_mutacji = 3
+masa_bialka_kda = 58.44
+
+# Wyświetlenie wartości zmiennych
+print("Gatunek:", gatunek)
+print("Analizowany gen:", gen)
+print("Liczba wykrytych mutacji:", liczba_mutacji)
+print("Masa białka [kDa]:", masa_bialka_kda)
+
+# Sprawdzenie typów danych za pomocą funkcji type()
+print("Typ zmiennej gatunek:", type(gatunek))
+print("Typ zmiennej gen:", type(gen))
+print("Typ zmiennej liczba_mutacji:", type(liczba_mutacji))
+print("Typ zmiennej masa_bialka_kda:", type(masa_bialka_kda))
+
+opis = "W genie " + gen + " wykryto mutacje."
+print(opis)
